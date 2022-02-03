bd38bac23b85cb338d9e9d1d6d2541d3171bc9db
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-02-03 18:04:25 -0300

    realização de alguns exercícios do dia, irei dar sequencia a partir de agora.

---
+++ fundamentos/bloco-4-introducao-javascript/4.2-JavaScript-Array-e-loop-For/exercicios4.2.js
@@ -0,0 +1,44 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+
+// let indexOfPortfolio = menu.indexOf('Portifólio');
+
+// console.log(indexOfPortfolio);
+
+// menu.push('Contato');
+
+// console.log(menu);
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (var index=0; index < cars.length; index+=1) {
+// console.log(cars[index]);
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (var index=0; index<groceryList.length; index+=1){
+//     console.log(groceryList[index]);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for (let nomes of names){
+//     nomes += 1;
+//     console.log(nomes);
+// }
+
+//exercícios do dia!
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (var index=0; index < numbers.length; index +=1){
+//      console.log(numbers[index]);
+// }
+let sum = 0;
+for (let index=0; index < numbers.length; index +=1){
+     sum = sum + numbers[index];
+}
+console.log(sum);
