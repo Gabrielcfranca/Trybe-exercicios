878ab9726aed8e9d77fe5a0329a2cdb5df0be014
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-02-03 19:36:32 -0300

    exercícioos do dia concluídos, faltou resolver o exercício 5, a segunda parte do exercício 6 e o exercício 7.

--- fundamentos/bloco-4-introducao-javascript/4.2-JavaScript-Array-e-loop-For/exercicios4.2.js
+++ fundamentos/bloco-4-introducao-javascript/4.2-JavaScript-Array-e-loop-For/exercicios4.2.js
@@ -32,13 +32,50 @@
 
 //exercícios do dia!
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impar =0;
+// let par =0;
+// for (var index=0; index < numbers.length; index +=1) {
+//      if (numbers[index]%2 ==1) {
+//           impar+=1;
+//      } else {
+//           par = 1;
+//      }
+// } console.log(impar);
+// console.log('Nenhum valor ímpar foi encontrado.')
 
 // for (var index=0; index < numbers.length; index +=1){
 //      console.log(numbers[index]);
 // }
-let sum = 0;
-for (let index=0; index < numbers.length; index +=1){
-     sum = sum + numbers[index];
+// let sum = 0;
+// let media = 0;
+// for (let index=0; index < numbers.length; index +=1){
+//      sum = sum + numbers[index];
+//           if (numbers[index] > 20) {
+//           console.log('Valor maior que 20.');
+//      } else {
+//           console.log('valor menor que 20.');
+//      }
+
+     
+// }
+// console.log('Soma dos elementos do array numbers');
+// console.log(sum);
+
+
+// console.log('Média dos elementos do array numbers');
+// media = sum/numbers.length;
+// console.log(media);
+
+// for (let index = 0; index > numbers; index +=1) {
+//       console.log(numbers[index]);
+
+//      }
+let cont = 0;
+let div = 0;
+for (let index=0; index < 25; index+=1){
+     cont = cont +1;
+     console.log(cont);
+     div = cont/2;
+     console.log("Divisao dos elementos por 2 " + div);
 }
-console.log(sum);
