994527054d895422e620599d208df8d61d36f8d8
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-02-08 12:12:13 -0300

    Exercícios da lissão 4.3.

--- fundamentos/bloco-4-introducao-javascript/4-3-JavaScript-Logica-de-Programacao-e-Algoritmos/4.3-logica.js
+++ fundamentos/bloco-4-introducao-javascript/4-3-JavaScript-Logica-de-Programacao-e-Algoritmos/4.3-logica.js
@@ -1,1 +1,33 @@
-10!
+
+// const num = 10;
+// let fatorial = [10];
+
+
+// for (let index = 1; index <= num; index +=1){
+//     fatorial = index * fatorial;
+  
+// }
+// console.log(fatorial);
+
+// let word = 'trybe';
+// let invertida = '';
+// for (let index = 0; index < word.length; index +=1){
+
+    // for (let index = word.length-1; index >= 0; index -=1){
+        
+    //     invertida += word[index];
+        
+    // }
+    // console.log (invertida);
+// }
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let letras = 0;
+
+for (let index = 0; index <= array.length; index +=1){
+    if (array.length[index] > array.length){
+    console.log(array[index]);
+    }
+    // letras += array[index];
+
+}  
