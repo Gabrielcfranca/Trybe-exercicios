8c980df73d245911775c35d96df795e0dd135d8b
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-02-09 18:50:44 -0300

    exercicios até a parte III do conteudo do dia 5.1 realizados, vou fazer agora os exercícios do dia, funcões com DOM!

---
+++ fundamentos/5-1-JavaScript-DOM-e-seletores/script.js
@@ -0,0 +1,13 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "lightsalmon";
+document.querySelectorAll(".emergency-tasks div")[0].style.border = "solid";
+document.querySelectorAll(".emergency-tasks div")[1].style.border = "solid";
+document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor = "red";
+document.querySelectorAll(".emergency-tasks div h3")[1].style.backgroundColor = "orange";
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "LightGreen";
+document.querySelectorAll(".no-emergency-tasks div")[0].style.border = "solid";
+document.querySelectorAll(".no-emergency-tasks div")[1].style.border = "solid";
+document.querySelectorAll(".no-emergency-tasks div h3")[0].style.backgroundColor = "DarkGreen";
+document.querySelectorAll(".no-emergency-tasks div h3")[1].style.backgroundColor = "SpringGreen";
