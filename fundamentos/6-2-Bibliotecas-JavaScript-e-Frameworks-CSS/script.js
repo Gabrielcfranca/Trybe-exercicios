01a7eef2071f700a7986ccc8b91e943a8333f619
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-02-22 15:51:19 -0300

    exercícios da aula 6.2

--- fundamentos/6-2-Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
+++ fundamentos/6-2-Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('#submitButton');  // busca o botao a ser cancelado o evento padrao
 let inputName = document.querySelector('#fullname'); //cria uma variável para receber o input Fullname
-const nameUser = document.querySelector('#nameUser'); //cria uma variável para receber o paragrafo de retonro.
+const nameUser = document.querySelector('#nameUser'); //cria uma variável para receber o paragrafo de retorno.
 
 function getUserValue (event) { //cria uma funcao com o eventDefault
     event.preventDefault();
@@ -9,3 +9,5 @@ function getUserValue (event) { //cria uma funcao com o eventDefault
 button.addEventListener('click', getUserValue); //funcao para acionar o botao e com o click acionar a funcao getUserValue
 
 
+document.getElementById('data').DatePickerX.init();
+window.DatePickerX.setDefaults( '#data' options )
