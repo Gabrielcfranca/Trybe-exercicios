9e13ded3b5dd919c5b27516fab60ec8383b46030
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-04-04 20:09:57 -0300

    comit do dia.

---
+++ front_End/bloco-10-Introducao-React/10-1-Hello-World/my_app/src/App.js
@@ -0,0 +1,14 @@
+import Hello  from './HelloWorld';
+import Header from './componentes/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hello />      
+    </div>
+  );
+}
+
+export default App;
