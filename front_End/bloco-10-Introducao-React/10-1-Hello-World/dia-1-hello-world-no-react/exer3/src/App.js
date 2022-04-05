ee37e80352d3f37a0d041b19d2e4b6825a8f8750
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-04-04 21:44:39 -0300

    exercicio3 do dia

---
+++ front_End/bloco-10-Introducao-React/10-1-Hello-World/dia-1-hello-world-no-react/exer3/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Content from './Content.jsx'
+import './App.css';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
